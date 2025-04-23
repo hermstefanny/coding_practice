bc8120005b5a4e721aa609f0e0345537f505c860
hermstefanny <aida.estefania.sosa@gmail.com>
2025-04-23 11:54:25 +0200

    algorithm modified for BestTimeToBuySell

--- Best_Time_to_Buy_Sell.py
+++ Best_Time_to_Buy_Sell.py
@@ -4,36 +4,25 @@ from typing import List
 def maxProfit(prices: List[int]) -> int:
 
     len_prices = len(prices)
-    if len_prices < 2:
+    if len_prices <= 1:
         return 0
-    i, j, k = 0, 1, 1
+    i = 0
+    win = 0
 
-    buy = prices[i]
-    sell = prices[j]
-
-    while j < len_prices:
-        if prices[j] < prices[i]:
-            buy = prices[j]
-            k = j + 1
-            break
+    while i < len_prices - 1:
+        j = i + 1
+        while j < len_prices - 1:
+            possible_win = prices[j] - prices[i]
+            if possible_win > win:
+                win = possible_win
+            j += 1
         i += 1
-        j += 1
-    print(buy)
 
-    print(i, j, k)
-    while k < len_prices:
-        if prices[k] > buy and prices[k] > sell:
-            sell = prices[k]
-
-        k += 1
-    print(k)
-
-    print(sell)
-    return max(sell - buy, 0)
+    return max(win, 0)
 
 
-print(f"El profit is {maxProfit([7, 1, 5, 3, 6, 4])}")
-print(f"El profit is {maxProfit([7, 6, 4, 3, 1])}")
-print(f"El profit is {maxProfit([1, 2])}")
-print(f"El profit is {maxProfit([1, 2, 4])}")
-print(f"El profit is {maxProfit([1, 4, 2])}")
+assert maxProfit([7, 1, 5, 3, 6, 4]), 5
+assert maxProfit([7, 6, 4, 3, 1]), 0
+assert maxProfit([1, 2]), 1
+assert maxProfit([1, 2, 4]), 3
+assert maxProfit([1, 4, 2]), 3
