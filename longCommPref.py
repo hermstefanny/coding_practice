5fd89c3893b89973ff42c1140c94cc78e2435d52
hermstefanny <aida.estefania.sosa@gmail.com>
2025-04-24 15:04:09 +0200

    third_attempt-

--- longCommPref.py
+++ longCommPref.py
@@ -4,8 +4,9 @@ from typing import List
 def longestCommonPrefix(strs: List[str]) -> str:
 
     sorted_list = sorted(strs, key=len)
+    pattern = sorted_list[0]
 
-    n = len(sorted_list[0])
+    n = len(pattern)
 
     short_list = []
 
@@ -15,24 +16,29 @@ def longestCommonPrefix(strs: List[str]) -> str:
     prefix_dict = {}
     prefix = ""
 
+    print(short_list)
+
     for word in short_list:
+        i = 0
         for letter in word:
-            prefix_dict[letter] = 1 + prefix_dict.get(letter, 0)
+            if letter == pattern[i]:
+                prefix_dict[letter] = 1 + prefix_dict.get(letter, 0)
+
+            i += 1
 
     print(prefix_dict)
 
     for key, value in prefix_dict.items():
-        if value == len(strs):
+        if value >= len(strs):
             prefix += key
         else:
             prefix += "-"
 
-    print(prefix)
-
     return prefix.split("-")[0]
 
 
-print(longestCommonPrefix(["flower", "flow", "flight"]))
-print(longestCommonPrefix(["dog", "racecar", "car"]))
-print(longestCommonPrefix(["cir", "car"]))
-print(longestCommonPrefix(["reflower", "flow", "flight"]))
+# print(longestCommonPrefix(["flower", "flow", "flight"]))
+# print(longestCommonPrefix(["dog", "racecar", "car"]))
+# print(longestCommonPrefix(["cir", "car"]))
+# print(longestCommonPrefix(["reflower", "flow", "flight"]))
+print(longestCommonPrefix(["aa", "aa"]))
