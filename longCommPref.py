4dd7d043a833900c41e974a418226760b38127b4
hermstefanny <aida.estefania.sosa@gmail.com>
2025-04-24 14:29:08 +0200

    seccond_attempt

--- longCommPref.py
+++ longCommPref.py
@@ -3,32 +3,36 @@ from typing import List
 
 def longestCommonPrefix(strs: List[str]) -> str:
 
-    if len(strs) <= 1:
-        return strs[0]
+    sorted_list = sorted(strs, key=len)
 
-    pattern = []
-    for letter in strs[0]:
-        pattern.append(letter)
+    n = len(sorted_list[0])
+
+    short_list = []
+
+    for word in sorted_list:
+        short_list.append(word[:n])
 
     prefix_dict = {}
     prefix = ""
 
-    for word in strs[1:]:
-        i = 0
+    for word in short_list:
         for letter in word:
-            if i >= len(pattern):
-                break
-            if letter == pattern[i]:
-                prefix_dict[letter] = 1 + prefix_dict.get(letter, 0)
+            prefix_dict[letter] = 1 + prefix_dict.get(letter, 0)
 
-            i += 1
+    print(prefix_dict)
 
     for key, value in prefix_dict.items():
-        if value == len(strs) - 1:
+        if value == len(strs):
             prefix += key
+        else:
+            prefix += "-"
 
-    return prefix
+    print(prefix)
+
+    return prefix.split("-")[0]
 
 
 print(longestCommonPrefix(["flower", "flow", "flight"]))
 print(longestCommonPrefix(["dog", "racecar", "car"]))
+print(longestCommonPrefix(["cir", "car"]))
+print(longestCommonPrefix(["reflower", "flow", "flight"]))
